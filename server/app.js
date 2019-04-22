2d104996334c5a6c67fbdd47974d4a6a450b9167
emwno <pulse.aashir@gmail.com>
2019-04-21 22:44:13 -0400

    setup backendless, body parser, session

--- server/app.js
+++ server/app.js
@@ -1,12 +1,42 @@
 var express = require('express');
+var expsession = require('express-session')
+var bodyParser = require('body-parser');
+var Backendless = require('backendless');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 
 var app = express();
 
+// Setup Backendless
+const APP_ID = '3C12364D-8C22-6E45-FFB7-08B271E5A300';
+const API_KEY = 'DEF1CFCE-2AE2-7E3D-FF3C-9A77DB377000';
+Backendless.initApp(APP_ID, API_KEY);
+
+// Setup access control
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Setup session
+app.use(expsession({
+    secret: 'un-ion',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+// Setup body parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// Setup routes
 indexRouter(app);
 loginRouter(app);
 
+// Setup listener
 const port = 5000;
 app.listen(port, () => `Server running on port ${port}`);
