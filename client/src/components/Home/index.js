63263faf8b5882b7d95d3bb24dc9d2d876df5765
Aahan Aggarwal <aahan.aggarwal99@gmail.com>
2019-04-22 15:25:04 -0400

    login and logout mods

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
-
+import axios from 'axios';
 class Home extends Component {
+    
+    constructor(props) {
+        super(props);
+        console.log(this);
+        this.state = {
+            titles: ""
+        };
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/login/check').then(function (response) {
+            console.log("logged in");
+        })
+        .catch(function (error) {
+            alert('redierect to login/register');
+            return;
+        });
+
+        axios.get('http://localhost:5000/').then(response => {
+            console.log(response);
+            this.setState({
+                titles: response.data[0].title
+            });
+        }).catch(function (error){
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
+                <p>{JSON.stringify(this.state.titles)}</p>
             </div>
         );
     }
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,22 +1,47 @@
 module.exports = function(app) {
+  
+  /* GET check logged in or not */
+  app.get("/login/check", function(req, res) {
+    let check = req.session.currentUser == null;
+    if (check == null) {
+      res.status(401);
+      res.send();
+    } else {
+      res.status(200);
+      res.send();
+    }
+  });
+
+  app.post("/logout", function(req, res) {
+    Backendless.UserService.logout().then(function(){
+      console.log("Logout successful");
+      req.session.currentUser = null;
+      res.status(200);
+      res.send();
+    }).catch(function(error){
+      console.log("logout unsuccessful!");
+      res.status(400);
+      res.send();
+    });
+    
+  });
 
   /* GET login listing. */
-  app.post('/login', function(req, res) {
-    console.log('POST /login');
+  app.post("/login", function(req, res) {
+    console.log("POST /login");
     Backendless.UserService.login(req.body.username, req.body.password, true)
       .then(function(loggedInUser) {
-        console.log('Login Successful');
+        console.log("Login Successful");
         req.session.currentUser = loggedInUser;
         res.status(200);
         res.send();
       })
       .catch(function(error) {
-        console.log('Login Unsuccessful: ' + error);
+        console.log("Login Unsuccessful: " + error);
         res.status(401);
         res.send();
-    });
+      });
 
     // res.send({ 'got': 'sdddd' });
   });
-
 };
