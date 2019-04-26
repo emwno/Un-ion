aaf28682b9eb0f6e7fe5483ca304682a1deabda4
emwno <pulse.aashir@gmail.com>
2019-04-25 23:15:01 -0400

    update user when at HOME

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,11 +1,21 @@
 module.exports = function(app) {
 	/* GET home listing. */
-	app.get('/', function(req, res, next) {
+	app.get('/', function(req, res) {
 		Backendless.UserService.getCurrentUser()
 			.then(currentUser => {
 				console.log('Fetched User: ' + currentUser.name);
-				res.status(200);
-				res.send(currentUser);
+
+				Backendless.Data.of('Users').findById(currentUser.objectId)
+					.then(updatedUser => {
+						console.log('Updated Fetched User');
+						res.status(200);
+						res.send(updatedUser);
+					})
+					.catch(error => {
+						console.log('Updated Fetched User Error');
+						res.status(401);
+						res.send();
+					});
 			})
 			.catch(error => {
 				console.log('Fetch User Error: ' + error);
